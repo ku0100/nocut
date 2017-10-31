6b21634e0a541f52dcdc99b991824a9b87a074f1
ku0100 <kuwilliams1@gmail.com>
2017-10-31 11:04:13 -0400

    sshClient will allow user to login to switch directly after looking up info.
    Will be able to use this with nocut and jack lookup tool cam is working on

--- sshClient.py
+++ sshClient.py
@@ -7,26 +7,27 @@
 import importlib.util
 import paramiko
 import getpass
+from nocut3 import *
 
 def sshClient(hostname, port=22):
-    self.hostname = hostname
-    self.port = port
     sshClient = paramiko.SSHClient()    # Create SSHClient instance
     # Below is policy that will automatically add missing host keys
     sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     sshClient.load_system_host_keys()
     username = input("username>>> ")
-    password = getpass.getpass(input("password>>> "))
-    client.connect(hostname, port, username, password)
+    password = getpass.getpass("password>>> ")
+    sshClient.connect(hostname, port, username, password)
     while True:
+        sshClient.connect(hostname, port, username, password)
         command = input(hostname + "#")
-        try:
-            if (command.lower() == "exit"):
-                print("Returning to nocut...")
-                nocut3.mainFunction()
+        print(command)
+        if (command.lower() == "exit"):
+            print("Returning to nocut...")
+            nocut3.mainFunction()
+            break
+        else:
             stdin, stdout, stderr = sshClient.exec_command(command)
             print(stdout.read())
             continue
-        except:
-            print("Invalid command!")
-            continue
+
+sshClient()
