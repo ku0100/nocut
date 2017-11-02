706c0f3d22e9e895f527e6d902fa31627d3dfc47
camwin <cameron.paulk@gmail.com>
2017-11-02 09:14:15 -0400

    Added code for IP address lookup for jack records
    Still returns a None at the end of the IPs, tho

--- ipinfo.py
+++ ipinfo.py
@@ -21,13 +21,40 @@ def pinnacle_get_switchport(jackID):
 	pinnacle_ports = open(pinnacle_ports_file, "r")
 	for i in pinnacle_ports:
 		if jackID in i:
-			ports = i.split()
-			pinnacle_ports.close()
 			# Found matching line with Jack ID, save the switch name, module number, and interface number
+                        ports = i.split()
 			module = ports[0][-1] # equal 1
 			switch = ports[0][:-2] # 003-1f-sw1
-			return switch + " " + module + "/" + ports[1] # 003-1f-sw1 1/14
-	return "Jack ID not active"
+			pinnacle_ports.close()
+                        return "Switch     : " + switch + "\n" + "Interface  : " + module + "/" + ports[1] # 003-1f-sw1 1/14)
+	pinnacle_ports.close()
+        return "Jack ID not active"
+
+def pinnacle_get_IP(jackID):
+
+        def print_ips(ipList):
+                        for eachIP in ipList:
+                                        print "IP: " + str(eachIP)
+	jackID = jackID.upper()
+	pinnacle_IP_file = pinnaclefile + "Export_IP_Assignments"
+	# Open Export_IP_Assignments file read-only
+	pinnacle_IPs = open(pinnacle_IP_file, "r")
+	# Create a list to add the IP addresses found to, since there may be >1
+        ipAddresses = []
+        for i in pinnacle_IPs:
+			if jackID in i:
+					line = i.split(',')
+					# Found matching line with Jack ID, return the IP address
+					ipAddresses.append(line[0])
+        # Close the file
+        pinnacle_IPs.close()
+        
+        # Check if we found any IPs and return them, or a message saying none were found
+        if not ipAddresses:
+	                return "Jack doesn't have an IP address provisioned"
+        else:
+                        return print_ips(ipAddresses)
+
 
 def find_vlan_id (vlan_name):
 
